8ef37caa45720967533faf401b0efe9edf992cf8
adil <adil.psg.aa@gmail.com>
2019-12-20 18:48:18 +0100

    AppStrucure
    Re-structured the app and add some features.

--- server.R
+++ server.R
@@ -1,8 +1,16 @@
 server = function(input, output, session) {
   
   output$plot1 <- renderPlotly(
-    plot_ly(type = 'scatter',mode='markers')
+    plot_ly(type = 'scatter',mode='markers') %>% layout(xaxis = list(title = "Mole X index"), yaxis = list(title = "Movment Time (s)"))
     
     
   )
+  
+  output$plot2 <- renderPlotly(
+    plot_ly(type = 'scatter',mode='markers') %>% layout(xaxis = list(title = "Mole X index"), yaxis = list(title = "Movment Time (s)"))
+    
+    
+  )
+  
+  
 }
--- ui.R
+++ ui.R
@@ -28,30 +28,60 @@ ui <- fluidPage(
               
               tabPanel(strong("Test Details"), 
                        
-                       br(),
-                       br(),
-                       br(),
-                       br(),
+                       sidebarLayout(
+                         # Inputs
+                         sidebarPanel(
+                           
+                           selectInput('in1', 'Choose Test', c(Choose='', state.name), selectize=FALSE),
+                           
+                           p("Test Details")
+                           #RebderText To generate the text
+                           
+                         ),
+                         
+                         
+                         
+                         
                        
-                       column(4, h4("Choose Test")
+                         
+                         
+                      
+                         mainPanel(
+                           
+                           plotlyOutput("plot1"),
+                           selectInput('in1', '', c(Choose='', state.name), selected = 'Index of Difficulty', selectize=FALSE)
+                         )
                        ),
                        
                        
-                       fluidRow(
-                         
-                         column(9,
-                                verbatimTextOutput('out'),
-                                selectInput('in1', 'Options', c(Choose='', state.name), selectize=FALSE)
-                         )
-                       )
+                       
+                       
               ),
               
-              tabPanel(strong("Compare Tests"), verbatimTextOutput("summary"))
-  ),
-  
-  mainPanel(
-    plotlyOutput("plot1")
-    
+              tabPanel(
+                strong("Compare Test"),
+                
+                sidebarLayout(
+                  # Inputs
+                  sidebarPanel(
+                    
+                    selectInput('in1', 'Test Type', c(Choose='',state.name), selectize=FALSE),
+                    
+                    p("Input To Compare : "),
+                    
+                    radioButtons("button",
+                                 "Compare Human Performance to..",
+                                 c("Eyes and Eyes tracker", "Nose and Mouse", "Right Hand and Mouse"))
+                    
+                  ),
+                  
+                  mainPanel(
+                    
+                    plotlyOutput("plot2"),
+                    selectInput('in1', '', c(Choose='', state.name), selectize=FALSE)
+                  )
+                ),
+                
+                )
   )
 )
-
